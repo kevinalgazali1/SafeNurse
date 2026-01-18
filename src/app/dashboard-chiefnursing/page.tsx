b8c55acfffb7cbe196a2bf8c1f35799887ef2ef2
RasyadBima15 <rasyadbimasatya04@gmail.com>
2026-01-18 12:00:29 +0800

    fix revisi in chief nursing

--- src/app/dashboard-chiefnursing/page.tsx
+++ src/app/dashboard-chiefnursing/page.tsx
@@ -283,6 +283,7 @@ export default function DashboardChiefNursing() {
     setSelectedKategori(selectedReport.kategori);
     setSelectedGrading(selectedReport.grading);
     setKronologi(selectedReport.kronologi);
+
   }, [selectedReport]);
 
   // Add CSS animations
